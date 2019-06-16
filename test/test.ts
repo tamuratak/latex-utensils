5ef91b7454376942f282e05a2d25813194513787
Takashi Tamura <tamuratak@users.noreply.github.com>
2019-06-16 13:57:42 +0900

    add tests

--- test/test.ts
+++ test/test.ts
@@ -7,9 +7,9 @@ suite('latexParser', () => {
     setup(() => {
       // .
     })
-  
+
     suite('parse', () => {
-        test('basic parse test', () => {            
+        test('basic parse test', () => {
             const tex = `
 \\begin{center}
 lmn
@@ -31,9 +31,48 @@ lmn
                 return
             }
             assert.equal(e.content, 'lmn')
+            assert.equal(e.location.start.line, 3)
+            assert.equal(e.location.start.column, 1)
+            assert.equal(e.location.end.line, 3)
+            assert.equal(e.location.end.column, 4)
         })
-        
+
+        test('parse newenvironment command', () => {
+            const tex = `\\newenvironment{newabc}
+{\\begin{abc}}
+{\\end{abc}}
+            `
+            const doc = latexParser.parse(tex)
+            const command = doc.content[0]
+            if (command.kind !== 'command') {
+                assert.fail()
+                return
+            }
+            assert.equal(command.name, 'newenvironment')
+            assert.equal(command.args.length, 3)
+        })
+
+        test('parse invalid commands without error', () => {
+            const tex = `\\begin{abc}`
+            const doc = latexParser.parse(tex)
+            const command = doc.content[0]
+            if (command === undefined) {
+                assert.fail()
+                return
+            }
+            if (command.kind !== 'command') {
+                assert.fail()
+                return
+            }
+            const arg = command.args[0]
+            const textString = arg.content[0]
+            if (textString.kind !== 'text.string') {
+                assert.fail()
+                return
+            }
+            assert.equal(textString.content, 'abc')
+        })
     })
-    
+
   })
-  
+
