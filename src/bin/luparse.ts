deb6a5306902dfab51177fb6607832f7c8e3b47a
Takashi Tamura <tamuratak@users.noreply.github.com>
2019-06-18 17:20:54 +0900

    add --disable-location option to src/bin/luparse.ts

--- src/bin/luparse.ts
+++ src/bin/luparse.ts
@@ -2,7 +2,26 @@ import * as parser from '../latex_parser'
 import * as fs from 'fs'
 import * as util from 'util'
 
-const filename = process.argv[2]
+function deleteLocation(node: any) {
+    if (node.hasOwnProperty('location')) {
+        delete node.location
+    }
+    for(const key of Object.getOwnPropertyNames(node)) {
+        const val = node[key]
+        if (typeof val === 'object') {
+            deleteLocation(val)
+        }
+    }
+}
+
+const argv = require('yargs').boolean('disable-location').argv
+const filename = argv._[0]
+console.log(filename)
 const s = fs.readFileSync(filename, {encoding: 'utf8'})
 const ret = parser.parse(s)
+
+if (argv['disable-location']) {
+    deleteLocation(ret)
+}
+
 console.log(util.inspect(ret, {showHidden: false, depth: null}))
