cf2a518704ec06478aafe5d507bceed66b4e0e3e
Takashi Tamura <tamuratak@users.noreply.github.com>
2019-06-23 19:32:57 +0900

    better error handling.

--- src/bin/luparse.ts
+++ src/bin/luparse.ts
@@ -1,6 +1,7 @@
 import * as parser from '../latex_parser'
 import * as fs from 'fs'
 import * as util from 'util'
+import * as process from 'process'
 import * as commander from 'commander'
 
 function deleteLocation(node: any) {
@@ -23,9 +24,25 @@ commander
 .option('-s, --start-rule [rule]', 'set start rule. default is "root".')
 .parse(process.argv)
 const filename = commander.args[0]
+if (!fs.existsSync(filename)) {
+    console.error(`${filename} not found.`)
+    process.exit(1)
+}
 const s = fs.readFileSync(filename, {encoding: 'utf8'})
 const startRule = commander.startRule || 'root'
-const ret = parser.parse(s, {startRule, enableComment: commander.comment})
+
+let ret: parser.LatexAst
+try {
+    ret = parser.parse(s, {startRule, enableComment: commander.comment})
+} catch (e) {
+    if (parser.isSyntaxError(e)) {
+        const loc = e.location
+        console.error(`SyntaxError at line: ${loc.start.line}, column: ${loc.start.column}.`)
+        console.error(e.message)
+        process.exit(1)
+    }
+    throw e
+}
 
 if (!commander.location) {
     deleteLocation(ret)
