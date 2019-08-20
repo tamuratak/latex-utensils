126e6541aa690c76f21847f95c027c43fb080652
Takashi Tamura <tamuratak@users.noreply.github.com>
2019-08-20 20:12:15 +0900

    enable timeout for bibexParser.parse

--- src/bibtex/bibtex_parser.ts
+++ src/bibtex/bibtex_parser.ts
@@ -1,2 +1,22 @@
-export * from './bibtex_parser_simple'
+import * as bp from './bibtex_parser_types'
+import * as _bibexParser from './bibtex_parser_simple'
+import * as _bibexParserWithTrace from './bibtex_parser_trace'
+import {ParserOptions} from '../pegjs/pegjs_types'
+import {TimeoutTracer} from '../pegjs/timeout'
+
 export * from './bibtex_parser_types'
+export {ParserOptions} from '../pegjs/pegjs_types'
+
+export function parse(s: string, option?: ParserOptions): bp.BibtexAst {
+    if (option && option.timeout && option.tracer) {
+        throw new Error('tracer and timeout not allowed at the same time.')
+    }
+    if (option && option.timeout) {
+        const tracer = new TimeoutTracer(option.timeout)
+        return _bibexParserWithTrace.parse(s, { tracer })
+    } else if (option && option.tracer) {
+        return _bibexParserWithTrace.parse(s, option)
+    } else {
+        return _bibexParser.parse(s, option)
+    }
+}
