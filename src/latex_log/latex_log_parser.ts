797030e51c3e63f4cfbbf8d89ab2801bec2d628b
Takashi Tamura <tamuratak@users.noreply.github.com>
2019-08-20 20:15:11 +0900

    enable timeout for latexLogParser.parse

--- src/latex_log/latex_log_parser.ts
+++ src/latex_log/latex_log_parser.ts
@@ -1,2 +1,22 @@
-export * from './latex_log_parser_simple'
+import * as llp from './latex_log_types'
+import * as _latexLogParser from './latex_log_parser_simple'
+import * as _latexLogParserWithTrace from './latex_log_parser_trace'
+import {ParserOptions} from '../pegjs/pegjs_types'
+import {TimeoutTracer} from '../pegjs/timeout'
+
 export * from './latex_log_types'
+export {ParserOptions} from '../pegjs/pegjs_types'
+
+export function parse(s: string, option?: ParserOptions): llp.LatexLogAst {
+    if (option && option.timeout && option.tracer) {
+        throw new Error('tracer and timeout not allowed at the same time.')
+    }
+    if (option && option.timeout) {
+        const tracer = new TimeoutTracer(option.timeout)
+        return _latexLogParserWithTrace.parse(s, { tracer })
+    } else if (option && option.tracer) {
+        return _latexLogParserWithTrace.parse(s, option)
+    } else {
+        return _latexLogParser.parse(s, option)
+    }
+}
